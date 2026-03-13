63d8062d6afdf5a62897e8d95e0c1a88af782293
angela chaj <angelachaj648@gmail.com>
2026-03-13 02:16:56 -0500

    datos estuidante

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿static void CantidadEstudiantes()
+{
+	Console.Write("Ingrese cantidad de estudiantes: ");
+	int n = int.Parse(Console.ReadLine());
+
+	Console.WriteLine("En la clase hay " + n + " estudiantes.");
+}
